944b924a56c3afe389c31b0a385d85bff3b70492
cryrobinson <cryrobinson@gmail.com>
2024-08-13 18:43:46 -0500

    Excercise 1 git commit

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -1,7 +1,28 @@
 # 1. Declare and assign the variables here:
+name_of_shuttle = "Determination"
+shuttle_speed_mph = 17500
+distance_mars_km = 225000000
+distance_moon_km = 384400
+miles_per_kilometer = 0.621
 
 # 2. Use print() to print the 'type' each variable. Print one item per line.
+print(type(name_of_shuttle))
+print(type(shuttle_speed_mph))
+print(type(distance_mars_km))
+print(type(distance_moon_km))
+print(type(miles_per_kilometer))
+
 
 # Code your solution to exercises 3 and 4 here:
+miles_to_mars = distance_mars_km * miles_per_kilometer
+hours_to_mars = miles_to_mars / shuttle_speed_mph
+days_to_mars = hours_to_mars / 24
+print(name_of_shuttle,"will take",days_to_mars,"days to reach Mars.")
 
 # Code your solution to exercise 5 here
+miles_to_moon = distance_moon_km * miles_per_kilometer
+hours_to_moon = miles_to_moon / shuttle_speed_mph
+days_to_moon = hours_to_moon / 24
+print(name_of_shuttle,"will take",days_to_moon,"days to reach the Moon.")
+print('Coding Rocks')
+print("Coding Rocks");
--- how-to-write-code/print-examples.py
+++ how-to-write-code/print-examples.py
@@ -3,4 +3,4 @@ print(2001);
 print("What","do","commas","do?");
 print("Does", "adding",      "space", "matter?");
 print('Launch' + 'Code');
-print("LaunchCode was founded in", 2013);
+print("LaunchCode\twas\nfounded\nin", 2013);
