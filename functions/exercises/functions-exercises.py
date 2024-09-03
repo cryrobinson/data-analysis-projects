3fc6ceabadbddf567de7089d5fc6023d9e507a17
cryrobinson <cryrobinson@gmail.com>
2024-09-03 17:34:06 -0500

    git commit

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -8,35 +8,35 @@ print(make_line(5))
 
 
 # # Part 1 B -- Make a Square
-# # create a function using your make_line function to code a square
-# def make_square(size):
-#     square = ""
-#     for i in range(size):
-#         square += (make_line(size) + "\n")
-#     return square
-# print(make_square(5))
+# create a function using your make_line function to code a square
+def make_square(size):
+    square = ""
+    for i in range(size):
+        square += (make_line(size) + "\n")
+    return square
+print(make_square(5))
 
 
 
 
-# # Part 1 C -- Make a Rectangle
-# def make_rectangle(width, height):
-#     rectangle = ""
-#     for i in range(height):
-#         rectangle += (make_line(width) + "\n")
-#     return rectangle
-# print(make_rectangle(5,3))
+# Part 1 C -- Make a Rectangle
+def make_rectangle(width, height):
+    rectangle = ""
+    for i in range(height):
+        rectangle += (make_line(width) + "\n")
+    return rectangle
+print(make_rectangle(5,3))
 
 
 
 
-# # Part 2 A -- Make a Stairs
-# def make_downward_stairs(height):
-#     stairs = ""
-#     for i in range(height):
-#         stairs += (make_line(i+1) + "\n")
-#     return stairs
-# print(make_downward_stairs(5)) 
+# Part 2 A -- Make a Stairs
+def make_downward_stairs(height):
+    stairs = ""
+    for i in range(height):
+        stairs += (make_line(i+1) + "\n")
+    return stairs
+print(make_downward_stairs(5)) 
 
 
 
